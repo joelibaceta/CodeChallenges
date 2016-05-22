349188355caa1d5a8038530d4a22c475df99c424
Joel Ibaceta <Niohnex@gmail.com>
2016-05-22 14:54:13 -0300

    Refactoring

--- Ch1/lib/duplicates.rb
+++ Ch1/lib/duplicates.rb
@@ -1,42 +1,26 @@
 class Duplicates
 
 	def self.findDuplicate(array)
-
-	  	@hash = Hash.new 
-
-	  	@i = 1
-
-	    array.each do |x|    	
-	    	@hash.has_key?(x) ? (return x) : @hash[x] = @i
-	    end
+		@hash = Hash.new  
+		array.map{|x| @hash[x] ? (return x) : @hash[x] = 1; 
 	end
 
   # Find the first non­repeating character in a string: ("DEFD" ­> E)
 
   	# First Method, this is fastest
   	def self.firstNonRepeating(s)
-
-	    characters = s.split("")
-	    repetitions =  Hash.new 
-	    nonrepeat = []
-	 
+ 
+	    	repetitions =  Hash.new  
 	 	beginning_time = Time.now
-	 	characters.each do |c|
-	 		if repetitions.has_key?(c)
-	 			repetitions[c] = repetitions[c] + 1
-	 		else
-	 			repetitions[c] = 1
-	 		end
-	 	end
-
-
-	 	repetitions.each do |k,v|
-	 		if v == 1
-	 			nonrepeat << k
-	 		end
-	 	end
-
-	 	# puts nonrepeat
+	 
+	 	characters.each_char {|c| repetitions[c] ? repetitions[c] += 1 : repetitions[c] = 1
+		 
+	 	# Cualquiera de estos dos pasos son ineficientes ya que se debe hacer un bucle por cada letra del texto
+	 	
+	 	result = repetitions.group_by{|k,v| v} # Agrupar el hash por valor
+	 	
+	 	puts result["1"].first imprimir la primera letra con una sola repeticion
+	 	
 	 	end_time = Time.now
 	 	puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
 	end
@@ -45,17 +29,17 @@ class Duplicates
 	def self.first_nonrepeat(s)
 		beginning_time = Time.now
 	   
-	    nonrepeat = []
-	    repeat = []
-	    s.each_char do |c|
-	        if !repeat.include?(c) && !nonrepeat.include?(c)
-	            nonrepeat << c
-	        elsif nonrepeat.include?(c)
-	            nonrepeat.delete(c)
-	            repeat << c
-	        end
-	    end
-	    end_time = Time.now
+	    	nonrepeat = []
+	    	repeat = []
+	    	s.each_char do |c|
+	        	if !repeat.include?(c) && !nonrepeat.include?(c)
+	            		nonrepeat << c
+	        	elsif nonrepeat.include?(c)
+	            		nonrepeat.delete(c)
+	            		repeat << c
+	        	end
+	    	end
+	    	end_time = Time.now
 
 	 	puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
 	    return !nonrepeat.empty? ? nonrepeat.first : ""    
